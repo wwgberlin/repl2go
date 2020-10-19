2a09cd181e3985f49b4a329189ef98c7a3df64c0
Sarah Köhler <sarah.c.koehler@gmail.com>
2020-10-19 20:59:24 +0200

    extract handler into package and create test for handler

---
+++ handler/handler.go
@@ -0,0 +1,38 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Message struct {
+	Hist History
+	Last string // "\n or \r\n " separated or whatever browser do to inputs
+}
+
+type History []struct {
+	Inputs string
+	Output []string //todo - this depends on what we get from the interpreter
+}
+
+func RunHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	var m Message
+	err := json.NewDecoder(r.Body).Decode(&m)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	// Get the history from the message
+	// Instaniate Interpreter instance : https://github.com/traefik/yaegi/tree/e32da38ad0fa87aced9812c391b57853225cb344#as-an-embedded-interpreter
+	// Iterate over the history
+	// We would extract the below to a new packages that wraps yaegi and also handles classifying commands (ie import statements))
+	// Invoke Interpreter(Yaegi) to evaluate each command in the history
+	// (BONUS) Compare return values from Yaegi with historical output
+	// Invoke Interpreter(Yaegi) on the Message.Last command
+	// Store the latest output
+	// Respond 200 where the body contains a new json with all the historical outputs and (at least) latest output & a dummy string for the valid go file
+}
---
+++ handler/handler_test.go
@@ -0,0 +1,27 @@
+package handler_test
+
+import (	
+	"net/http"	
+	"net/http/httptest"	
+	"testing"
+	"github.com/wwgberlin/repl2go/handler"
+)
+
+func TestRunHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/ru", nil)
+	if err != nil {	
+		t.Fatal(err)	
+	}	
+			
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(handler.RunHandler)	
+
+	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method	
+	// directly and pass in our Request and ResponseRecorder.	
+	handler.ServeHTTP(rr, req)	
+
+	// Check the status code is what we expect.	
+	if status := rr.Code; status != http.StatusNotFound {		
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusNotFound);
+	}
+}
